db5a1a7bac5f0b7fa48a80d857827fa209f3c5bb
Ryan Papsin <ryanpapsin175@gmail.com>
2022-11-29 10:55:47 -0700

    after review changes

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,10 +1,10 @@
-import React from "react"
+import React from "react";
 
 class HornedBeast extends React.Component {
   render() {
     return (<div>
       <h2>Horned Beast {this.props.title}</h2>
-      <img src= {this.props.imageUrl} alt='cool horned beast'></img>
+      <img src={this.props.imageUrl} alt={this.props.description}></img>
       <p>Animal Description:{this.props.description}</p>
     </div>
     )
